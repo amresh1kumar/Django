77ca20b6aa1212355fd26020046cf1c7b3425628
amresh1kumar <amreshbkp0111@gmail.com>
2025-08-06 22:03:29 +0530

    ok

---
+++ GroceryShop/Grocery_Shop/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+
+def Home(request):
+   return HttpResponse('Hello Django')
+
+
+def Page(request):
+   return render(request,'index.html')
